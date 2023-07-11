c2e1a670e69a9b8e2fa01671293f665bef717e0c
Sandro André Janotte <sjanotte@gmail.com>
2023-07-10 21:14:08 -0300

    Updating a person

--- src/main/java/br/net/supptech/personservice/controllers/PersonController.java
+++ src/main/java/br/net/supptech/personservice/controllers/PersonController.java
@@ -2,6 +2,7 @@ package br.net.supptech.personservice.controllers;
 
 import br.net.supptech.personservice.models.PersonModel;
 import br.net.supptech.personservice.services.PersonService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,5 +40,15 @@ public class PersonController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person not found!");
         }
         return ResponseEntity.status(HttpStatus.OK).body(optionalPersonModel.get());
+    }
+
+    @PutMapping("/{personId}")
+    public ResponseEntity<?> updatePerson(@PathVariable UUID personId, @RequestBody PersonModel personModel) {
+        Optional<PersonModel> optionalPersonModel = personService.findPersonById(personId);
+        if (optionalPersonModel.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person not found!");
+        }
+        BeanUtils.copyProperties(personModel, optionalPersonModel.get(), "personId");
+        return ResponseEntity.status(HttpStatus.OK).body(personService.savePerson(optionalPersonModel.get()));
     }
 }
